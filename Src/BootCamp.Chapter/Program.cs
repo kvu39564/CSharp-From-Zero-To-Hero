aa0ca0558fa9789a74ddf543219fbc7a82cf30f2
Kevin Vu <kvu39564@gmail.com>
2020-07-18 14:07:15 -0400

    Homework 1 Changes

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,34 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName;
+            string lastName;
+            int age;
+            int weight;
+            float height;
+            float bmi;
+
+            Console.Write("Enter your first name: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Enter your last name: ");
+            lastName = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your weight (kg): ");
+            weight = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your height (cm): ");
+            height = float.Parse(Console.ReadLine());
+
+            bmi = (float)(weight / Math.Pow((height / 100.0), 2));
+
+            Console.WriteLine("\n" + firstName + " " + lastName + " is " + age + " years old, " +
+                              "his weight is " + weight + " kg " +
+                              "and his height is " + height + " cm.\n");
+            Console.WriteLine(firstName + "'s BMI is " + bmi);
         }
     }
 }
